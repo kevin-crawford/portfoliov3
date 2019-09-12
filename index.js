bb3179a88785cdacc00b0576d5e2582e70c3e02c
Kevin Crawford <kcdouglas92@gmail.com>
2019-09-12 13:34:53 -0700

    added new menu styling and animation, updated timeline icon colors and text

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-$(function() {
+$(function () {
   console.log("loading jquery");
-  $(window).on("scroll", function() {
+  $(window).on("scroll", function () {
     if ($(window).scrollTop() > 50) {
-      $(".header").addClass("active");
+      $(".navbar").addClass("content-view");
     } else {
-      $(".header").removeClass("active");
+      $(".navbar").removeClass("content-view");
     }
   });
 });
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.querySelector('.menu-btn');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+})
